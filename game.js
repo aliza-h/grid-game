18f19b7db3cc5c950723c54b0bec066af3c7bee5
Aliza Howe <alizahowe26@gmail.com>
2022-03-09 09:08:30 -0500

    VERY basic working visuals

--- game.js
+++ game.js
@@ -5,7 +5,16 @@
 // row x col
 
 let gameBoard = document.getElementById("game-board");
-let gameRow = document.createElement("div");
-gameRow.className = "game-row";
 
-gameBoard.appendChild(gameRow);
+for (let row = 0; row < 10; row++) {
+    let gameRow = document.createElement("div");
+    gameRow.className = "game-row";
+    gameBoard.appendChild(gameRow);
+
+    for (let cell = 0; cell < 10; cell++) {
+        let gameCell = document.createElement("div");
+        gameCell.className = "game-cell";
+        gameRow.appendChild(gameCell);
+    }
+
+}
